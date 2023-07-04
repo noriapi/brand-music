733414174b96d07010aaff6a2d1c57aa109641c1
noriapi <70106808+noriapi@users.noreply.github.com>
2023-07-04 09:22:53 +0900

    fix: fix exports (#16)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 export { from as ic, fromNum as mayIc } from "./ic.js";
+export * as IC from "./ic.js";
 export { fromNum as mayMnn, from as mnn } from "./mnn.js";
+export * as MNN from "./mnn.js";
 export { fromNum as mayPc, from as pc } from "./pc.js";
+export * as PC from "./pc.js";
 export { from as pcs } from "./pcs.js";
+export * as PCS from "./pcs.js";
 export { fromNum as mayPi, from as pi } from "./pi.js";
+export * as PI from "./pi.js";
 export { fromNum as mayPic, from as pic } from "./pic.js";
+export * as PIC from "./pic.js";
 export { from as pics } from "./pics.js";
+export * as PICS from "./pics.js";
 export { from as semitones } from "./semitones.js";
+export * as S from "./semitones.js";
