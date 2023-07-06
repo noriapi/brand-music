0a73ad7e1a906ed48cec35092c2822530bb6f583
noriapi <70106808+noriapi@users.noreply.github.com>
2023-07-06 21:38:54 +0900

    fix: make makeAbs handle NaN (#44)

--- src/refined/non-negative.ts
+++ src/refined/non-negative.ts
@@ -7,5 +7,20 @@ export type NonNegative = number & NonNegativeBrand;
 export const isNonNegative = <T>(v: T): v is T & NonNegative =>
   typeof v === "number" && v >= 0;
 
-export const makeAbs = <T extends number>(v: T) =>
-  Math.abs(v) as T & NonNegative;
+export function makeAbs<T extends number, N extends NonNegative>(
+  v: T,
+  edgeCases: { NaN: N }
+): (T & NonNegative) | N;
+export function makeAbs<T extends number>(
+  v: T,
+  edgeCases?: { NaN?: never }
+): (T & NonNegative) | (0 & NonNegative);
+export function makeAbs(v: number, edgeCases?: { NaN?: NonNegative }) {
+  const caseNaN = edgeCases?.NaN ?? (0 as 0 & NonNegative);
+
+  if (Number.isNaN(v)) {
+    return caseNaN;
+  } else {
+    return Math.abs(v);
+  }
+}
