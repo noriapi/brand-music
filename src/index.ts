a926b0d90075627e55631d763b7afbf603bc28f9
noriapi <70106808+noriapi@users.noreply.github.com>
2023-07-07 23:45:19 +0900

    feat: add understandable module name

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-export * as IC from "./ic.js";
-export * as MNN from "./mnn.js";
-export * as PC from "./pc.js";
-export * as PCS from "./pcs.js";
-export * as PI from "./pi.js";
-export * as PIC from "./pic.js";
-export * as PICS from "./pics.js";
-export * as S from "./semitones.js";
+/* eslint-disable simple-import-sort/exports */
+
+import * as MidiNoteNumber from "./mnn.js";
+export { MidiNoteNumber, MidiNoteNumber as MNN };
+
+import * as PitchClass from "./pc.js";
+export { PitchClass, PitchClass as PC };
+
+import * as PitchClassSet from "./pcs.js";
+export { PitchClassSet, PitchClassSet as PCS };
+
+import * as PitchInterval from "./pi.js";
+export { PitchInterval, PitchInterval as PI };
+
+import * as Semitones from "./semitones.js";
+export { Semitones, Semitones as S };
+
+import * as PitchIntervalClass from "./pic.js";
+export { PitchIntervalClass, PitchIntervalClass as PIC };
+
+import * as PitchIntervalClassSet from "./pics.js";
+export { PitchIntervalClassSet, PitchIntervalClassSet as PICS };
+
+import * as IntervalClass from "./ic.js";
+export { IntervalClass, IntervalClass as IC };
