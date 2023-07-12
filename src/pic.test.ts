33e84be0757db2624eccde5f743876190a210664
noriapi <70106808+noriapi@users.noreply.github.com>
2023-07-12 21:28:40 +0900

    test(pic): add test for PIC.fromPi (#56)

--- src/pic.test.ts
+++ src/pic.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 
+import type { PitchInterval } from "./pi.js";
 import * as PIC from "./pic.js";
 
 describe("markNum", () => {
@@ -17,3 +18,20 @@ describe("markNum", () => {
     expect(PIC.markNum(Number.POSITIVE_INFINITY)).toBe(false);
   });
 });
+
+it.each([
+  [-13, 11],
+  [-12, 0],
+  [-11, 1],
+  [-1, 11],
+  [0, 0],
+  [1, 1],
+  [11, 11],
+  [12, 0],
+  [13, 1],
+] as [pi: PitchInterval, pic: PIC.PitchIntervalClass][])(
+  "fromPi(%i) -> %i",
+  (pi, expected) => {
+    expect(PIC.fromPi(pi)).toBe(expected);
+  }
+);
